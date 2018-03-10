541a0f83c3a9243ce78c3940d23725521b82778a
小山 由記 <oja_yuki@Mac-mini.local>
2018-03-10 10:45:36 +0900

    first

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,59 @@
+class WordsController < ApplicationController
+  def index
+    @words = Word.all
+  end
+
+  def show
+    @word = Word.find(params[:id])
+  end
+
+  def new
+    @word = Word.new(content: 'ご')
+  end
+
+  def create
+    user = User.first
+    s = session[:user_id]
+    binding.pry
+    @word = user.words.build(word_params)
+
+    if @word.save
+      flash[:success] = '尻取りが正常に投稿されました'
+      redirect_to @word
+    else
+      flash.now[:danger] = '尻取りが投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @word = Word.find(params[:id])
+  end
+
+  def update
+    @word = Word.find(params[:id])
+
+    if @word.update(word_params)
+      flash[:success] = '尻取りは正常に更新されました'
+      redirect_to @word
+    else
+      flash.now[:danger] = '尻取りは更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @word = Word.find(params[:id])
+    @word.destroy
+
+    flash[:success] = '尻取りは正常に削除されました'
+    redirect_to words_url
+   end
+
+  private
+
+  # Strong Parameter
+  def word_params
+    params.require(:word).permit(:content)
+  end
+end
---
+++ app/models/word.rb
@@ -0,0 +1,6 @@
+class Word < ApplicationRecord
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 255 }
+end
