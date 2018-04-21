2b06c779a01332c6700ff0b85d21c3b9a2b010e2
小山 由記 <>
2018-04-22 08:48:52 +0900

    日本語熱、ユーザネーム変更可

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414001024) do
+ActiveRecord::Schema.define(version: 20180421225419) do
 
   create_table "kugens", force: :cascade do |t|
     t.integer "yourei_id_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180414001024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "ondo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
