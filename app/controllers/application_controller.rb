352e6f5e1c524e26f64f6624fcb1c7df29166fbc
小山 由記 <>
2018-04-22 09:09:59 +0900

    userモデルに、ondo追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :ondo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :ondo])
   end
 end
